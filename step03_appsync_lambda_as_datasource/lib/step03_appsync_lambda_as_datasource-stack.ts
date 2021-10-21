f19b766de161790a84d0c49691571b04978c9491
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-10-21 06:16:36 +0500

    Create gql schema

--- step03_appsync_lambda_as_datasource/lib/step03_appsync_lambda_as_datasource-stack.ts
+++ step03_appsync_lambda_as_datasource/lib/step03_appsync_lambda_as_datasource-stack.ts
@@ -1,4 +1,6 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class Step03AppsyncLambdaAsDatasourceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
